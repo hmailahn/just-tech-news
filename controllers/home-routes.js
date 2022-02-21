6929bce07081333797dc8a57afc3ab18cae5b1eb
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-21 16:24:17 -0600

    added log in and log  out functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,4 +37,29 @@ router.get('/', (req, res) => {
         });
     });
 
+    router.get('/login', (req, res) => {
+        if (req.session.loggedIn) {
+            res.redirect('/');
+            return;
+          }
+        
+          res.render('login');
+        });
+      router.get('/', (req, res) => {
+        console.log(req.session);
+      
+        // other logic...
+      });
+
+      router.post('/logout', (req, res) => {
+        if (req.session.loggedIn) {
+            req.session.destroy(() => {
+              res.status(204).end();
+            });
+          }
+          else {
+            res.status(404).end();
+          }
+    });
+
 module.exports = router;
